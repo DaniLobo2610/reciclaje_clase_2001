a97f21b125b2de4e2572690114df3861fd671c40
DaniLobo2610 <137654960+DaniLobo2610@users.noreply.github.com>
2026-06-23 10:36:33 -0600

    Update app.py

--- app.py
+++ app.py
@@ -7,5 +7,10 @@ from PIL import Image
 
 st.set_page_config(page_title="Reciclaje IA-ISC", layout="centered")
 st.title("Modelo Predictivo Reciclaje clase de IA-ISC-Campus Comayagua-2026")
-st.title("Alberto Daniel Lobo Chavarria")
-st.write("Suba una imagen para clasificar con el modelo MobilnetV2 pre entrenado", layout="centered")
+st.write("Suba una imagen para clasificar con el modelo MobilnetV2 pre entrenado")
+st.write("ALBERTO DANIEL LOBO CHAVARRIA")
+
+IMG_SIZE=(224,224)
+MODEL_DIR=Path("Modelo_reciclaje_mobilnet")
+CLASS_PATH=MODEL_DIR/"waste_mobilnet.json"
+MOEL_PATHS=[MODEL_DIR/"waste_mobilnet.h5", MODELO_DIR/"waste_mobilnet.keras"]
