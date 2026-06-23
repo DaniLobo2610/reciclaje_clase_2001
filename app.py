e560ef3f506b63ec6cbece7a1049a98e430c617a
DaniLobo2610 <137654960+DaniLobo2610@users.noreply.github.com>
2026-06-23 10:29:05 -0600

    Update 2 app.py

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ import streamlit as st
 import tensorflow as tf
 from PIL import Image
 
-st.set_page_config(page_title="Reciclaje IA-ISC". layout="centered")
+st.set_page_config(page_title="Reciclaje IA-ISC", layout="centered")
 st.title("Modelo Predictivo Reciclaje clase de IA-ISC-Campus Comayagua-2026")
 st.write("Suba una imagen para clasificar con el modelo MobilnetV2 pre entrenado")
